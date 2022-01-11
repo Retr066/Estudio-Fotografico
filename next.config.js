3301087c6754d7d3eb9f706cdd5a68f2b4a3a3d1
Jherson Lopez Perez <jherson.lopez88@gmail.com>
2022-01-11 01:13:27 -0500

    save

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
     domains: ["res.cloudinary.com"],
   },
   env: {
-    SENDGRID_API_KEY:
-      "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",
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   },
 };
